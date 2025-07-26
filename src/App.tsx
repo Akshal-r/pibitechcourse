080586305f013088b248bcb763f483b6febd2532
Akshal-r <akshalr920822104009@nprcolleges.org>
2025-07-26 14:48:12 +0530

    Update index.html and App.tsx with layout and UI improvements

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ const queryClient = new QueryClient();
 const App = () => (
   <HelmetProvider>
     <Helmet>
-      <title>AI Product Development Program</title>
       <link rel="icon" href="/favicon.ico" />
     </Helmet>
     <QueryClientProvider client={queryClient}>
